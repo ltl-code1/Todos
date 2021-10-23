fbea31fb7397b9c2271d73a326e03c002d50db3c
tonglei <ltl_code@163.com>
2021-10-23 23:28:03 +0800

    create

---
+++ components/Addition/Addition.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<text>当前最新的count值为：{{$store.state.count}}</text>
+		<button @click="$store.commit('add')">+1</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"Addition",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,18 +1,23 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<Addition />
+		<Subtraction />
 	</view>
 </template>
 
 <script>
+	import Addition from '../../components/Addition/Addition.vue'
+	import Subtraction from '../../components/Subtraction/Subtraction.vue'
+	
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
+		},
+		components: {
+			Addition,
+			Subtraction
 		},
 		onLoad() {
 
@@ -24,29 +29,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>
